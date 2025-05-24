6c56b8b096d71efe4962ab1e8146f59cf29287a9
Dmytro Zykov <dmytri.zykov@gmail.com>
2025-05-24 12:11:44 +0300

    Update book repository type

--- Sources/Model/BookRepository.swift
+++ Sources/Model/BookRepository.swift
@@ -2,125 +2,118 @@ import Combine
 import CoreData
 import Foundation
 
-protocol BookRepository {
-    func fetchBooks() -> AnyPublisher<[BookModel], BookRepositoryError>
-    func update(book: BookModel) -> AnyPublisher<Void, BookRepositoryError>
-    func add(book: BookModel) -> AnyPublisher<Void, BookRepositoryError>
-    func delete(book: BookModel) -> AnyPublisher<Void, BookRepositoryError>
+protocol BookRepositoryProtocol {
+    func fetchBooks() -> AnyPublisher<[BookModel], Error>
+    func update(book: BookModel) -> AnyPublisher<Void, Error>
+    func add(book: BookModel) -> AnyPublisher<Void, Error>
+    func delete(book: BookModel) -> AnyPublisher<Void, Error>
 }
 
-enum BookRepositoryError: LocalizedError {
-    case bookNotFound(book: BookModel)
-    case fetchFailed(underlying: Error)
-    case saveFailed(underlying: Error)
+enum BookRepositoryError: Error, LocalizedError {
+    case bookNotFound
+    
+    var errorDescription: String? {
+        switch self {
+        case .bookNotFound:
+            return "Book not found"
+        }
+    }
 }
 
-final class CoreDataBookRepository: BookRepository {
+final class BookRepository: BookRepositoryProtocol {
     private let context: NSManagedObjectContext
     
     init(context: NSManagedObjectContext) {
         self.context = context
     }
     
-    func fetchBooks() -> AnyPublisher<[BookModel], BookRepositoryError> {
-        Deferred {
-            Future { promise in
-                self.context.perform {
-                    let request = Book.fetchRequest()
-                    request.sortDescriptors = [.init(keyPath: \Book.title, ascending: true)]
-                    
-                    do {
-                        let books = try self.context.fetch(request)
-                        promise(.success(books.compactMap { $0.bookModel }))
-                    } catch {
-                        promise(.failure(.fetchFailed(underlying: error)))
-                    }
+    func fetchBooks() -> AnyPublisher<[BookModel], Error> {
+        Future { promise in
+            self.context.perform {
+                let request = Book.fetchRequest()
+                request.sortDescriptors = [.init(keyPath: \Book.title, ascending: true)]
+                
+                do {
+                    let books = try self.context.fetch(request)
+                    promise(.success(books.compactMap { $0.bookModel }))
+                } catch {
+                    promise(.failure(error))
                 }
             }
         }
-        .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
     }
     
-    func update(book: BookModel) -> AnyPublisher<Void, BookRepositoryError> {
-        Deferred {
-            Future { promise in
-                self.context.perform {
-                    let request = Book.fetchRequest()
-                    request.predicate = NSPredicate(format: "id == %@", book.id as CVarArg)
+    func update(book: BookModel) -> AnyPublisher<Void, Error> {
+        Future { promise in
+            self.context.perform {
+                let request = Book.fetchRequest()
+                request.predicate = NSPredicate(format: "id == %@", book.id as CVarArg)
+                
+                do {
+                    let books = try self.context.fetch(request)
                     
-                    do {
-                        let books = try self.context.fetch(request)
-                        
-                        guard let bookEntity = books.first else {
-                            promise(.failure(.bookNotFound(book: book)))
-                            return
-                        }
-                        
-                        bookEntity.title = book.title
-                        bookEntity.author = book.author
-                        bookEntity.isbn = book.isbn
-                        bookEntity.pageCount = Int16(book.pageCount)
-                        bookEntity.currentPage = Int16(book.currentPage)
-                        
-                        try self.context.save()
-                        promise(.success(()))
-                    } catch {
-                        promise(.failure(.saveFailed(underlying: error)))
+                    guard let bookEntity = books.first else {
+                        promise(.failure(BookRepositoryError.bookNotFound))
+                        return
                     }
+                    
+                    bookEntity.title = book.title
+                    bookEntity.author = book.author
+                    bookEntity.isbn = book.isbn
+                    bookEntity.pageCount = Int16(book.pageCount)
+                    bookEntity.currentPage = Int16(book.currentPage)
+                    
+                    try self.context.save()
+                    promise(.success(()))
+                } catch {
+                    promise(.failure(error))
                 }
             }
         }
-        .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
     }
     
-    func add(book: BookModel) -> AnyPublisher<Void, BookRepositoryError> {
-        Deferred {
-            Future { promise in
-                self.context.perform {
-                    let newBook = Book(context: self.context)
-                    newBook.id = book.id
-                    newBook.title = book.title
-                    newBook.author = book.author
-                    newBook.pageCount = Int16(book.pageCount)
-                    newBook.currentPage = Int16(book.currentPage)
-                    
-                    do {
-                        try self.context.save()
-                        promise(.success(()))
-                    } catch {
-                        promise(.failure(.saveFailed(underlying: error)))
-                    }
+    func add(book: BookModel) -> AnyPublisher<Void, Error> {
+        Future { promise in
+            self.context.perform {
+                let newBook = Book(context: self.context)
+                newBook.id = book.id
+                newBook.title = book.title
+                newBook.author = book.author
+                newBook.pageCount = Int16(book.pageCount)
+                newBook.currentPage = Int16(book.currentPage)
+                
+                do {
+                    try self.context.save()
+                    promise(.success(()))
+                } catch {
+                    promise(.failure(error))
                 }
             }
         }
-        .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
     }
     
-    func delete(book: BookModel) -> AnyPublisher<Void, BookRepositoryError> {
-        Deferred {
-            Future { promise in
-                self.context.perform {
-                    let request = Book.fetchRequest()
-                    request.predicate = NSPredicate(format: "id == %@", book.id as CVarArg)
-                    
-                    do {
-                        guard let bookToDelete = try self.context.fetch(request).first else {
-                            promise(.failure(.bookNotFound(book: book)))
-                            return
-                        }
-                        
-                        self.context.delete(bookToDelete)
-                        try self.context.save()
-                    } catch {
-                        promise(.failure(.saveFailed(underlying: error)))
+    func delete(book: BookModel) -> AnyPublisher<Void, Error> {
+        Future { promise in
+            self.context.perform {
+                let request = Book.fetchRequest()
+                request.predicate = NSPredicate(format: "id == %@", book.id as CVarArg)
+                
+                do {
+                    guard let bookToDelete = try self.context.fetch(request).first else {
+                        promise(.failure(BookRepositoryError.bookNotFound))
+                        return
                     }
+                    
+                    self.context.delete(bookToDelete)
+                    try self.context.save()
+                } catch {
+                    promise(.failure(error))
                 }
             }
         }
-        .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
     }
 }
