933d1727466599aa139b895cef60af3a7a9dc67b
Dmytro Zykov <dmytri.zykov@gmail.com>
2025-05-24 06:34:27 +0300

    Extend model

--- Sources/Model/BookModel.swift
+++ Sources/Model/BookModel.swift
@@ -3,6 +3,7 @@ import CoreData
 struct BookModel: Identifiable, Hashable {
     let id: UUID
     let title: String
+    let author: String
     let isbn: String
     let pageCount: Int
     var currentPage: Int
@@ -10,6 +11,7 @@ struct BookModel: Identifiable, Hashable {
     static let preview: BookModel = .init(
         id: UUID(),
         title: "The Alchemist",
+        author: "Paulo Coelho",
         isbn: "9780062564811",
         pageCount: 224,
         currentPage: 12
@@ -21,6 +23,7 @@ extension Book {
         .init(
             id: self.id ?? UUID(),
             title: self.title ?? "",
+            author: self.author ?? "",
             isbn: self.isbn ?? "",
             pageCount: Int(self.pageCount),
             currentPage: Int(self.currentPage)
